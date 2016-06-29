779f6dae2ef34ae7c323c9450fee8950d1629007
GaganJagadeesha <gaganreddy.gagz@gmail.com>
2016-06-29 18:06:43 -0400

    One plus Two

--- romancaltests.c
+++ romancaltests.c
@@ -11,8 +11,17 @@ void RomanNumeralCalculatorOneplusOne(void) {
 	assert(strcmp(strres,add(str1,str2))==Equal);
 	
  }
+
+void RomanNumeralCalculatorOneplusTwo(void) {
+	char str1[] = "I";
+	char str2[] = "II";
+	char strres[] = "III";
+	assert(strcmp(strres,add(str1,str2))==Equal);
+	
+ }
 int main(void) {
 	RomanNumeralCalculatorOneplusOne();
+	RomanNumeralCalculatorOneplusTwo();
 	puts("All tests passed");
 	return 0;
  }
